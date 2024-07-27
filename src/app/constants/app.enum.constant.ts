783a8abae04b7909d41576583d979dc873759b63
ngoctienle <ngoctienle.2399@gmail.com>
2024-07-27 15:06:52 +0700

    feat(*): init nest packages, update eslintignore

---
+++ src/app/constants/app.enum.constant.ts
@@ -0,0 +1,19 @@
+export enum ENUM_APP_ENVIRONMENT {
+  PRODUCTION = 'production',
+  STAGING = 'staging',
+  DEVELOPMENT = 'development'
+}
+
+export enum ENUM_APP_TIMEZONE {
+  ASIA_SINGAPORE = 'Asia/Singapore',
+  ASIA_JAKARTA = 'Asia/Jakarta'
+}
+
+export enum ENUM_APP_STATUS_CODE_ERROR {
+  UNKNOWN_ERROR = 5040
+}
+
+export enum ENUM_MESSAGE_LANGUAGE {
+  EN = 'en',
+  VI = 'vi'
+}
